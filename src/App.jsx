e105244aa0cfb52e393f1a31aa1fe501c74e19b5
isha-web1 <isha.web1@gmail.com>
2023-05-11 22:30:31 +0600

    all pakage init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-purple-600 font-semibold'>Vite + React</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
