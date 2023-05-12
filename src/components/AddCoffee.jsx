45920ef8e15bcad9c115024edae25565d9187ddb
isha-web1 <isha.web1@gmail.com>
2023-05-12 19:08:37 +0600

    mongodb added

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,10 +1,47 @@
 import React from "react";
+import Swal from 'sweetalert2'
 
 const AddCoffee = () => {
+
+  const handleAddCoffee = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+
+    const newCoffee = {name, quantity, supplier, taste, category, details, photo}
+    console.log(newCoffee)
+
+    fetch('http://localhost:5000/coffee',{
+      method : 'POST',
+      headers : {
+        'content-type' : 'application/json'
+      },
+      body : JSON.stringify(newCoffee)
+    })
+    .then(res => res.json())
+    .then(data =>{
+      console.log(data)
+      if(data.insertedId){
+        Swal.fire({
+          title: 'success',
+          text: 'user added successfully',
+          icon: 'success',
+          confirmButtonText: 'ok'
+        })
+
+      }
+    })
+  }
   return (
     <div className="p-24">
       <h1 className="text-4xl font-bold text-center text-purple-600">Add a Coffee</h1>
-      <form>
+      <form onSubmit={handleAddCoffee}>
         <div className="md:flex mb-8">
         <div className="form-control md:w-1/2">
           <label className="label">
